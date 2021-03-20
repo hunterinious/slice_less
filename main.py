69d1c0910b29efbb8b80b7154d5750bae8e4f7cf
Vladyslav Nerodenko <vladyslavnerodenko@Vladyslavs-MacBook-Pro.local>
2021-03-20 20:17:05 +0200

    params names updated

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-def slice_less1(li, lesser):
-    li = [l for l in li if l > lesser]
-    return sorted(li, reverse=True)
+def slice_less1(my_list, lesser):
+    my_list = [l for l in my_list if l > lesser]
+    return sorted(my_list, reverse=True)
 
 
-def slice_less2(li, lesser):
-    sorted_li = sorted(li, reverse=True)
-    for i, l in enumerate(sorted_li):
+def slice_less2(my_list, lesser):
+    sorted_list = sorted(my_list, reverse=True)
+    for i, l in enumerate(sorted_list):
         if l < lesser:
-            return sorted_li[:i - 1]
+            return sorted_list[:i - 1]
 
 
-li = [8, 9, 7, 0 , -2 , 3, -1, -2, 5, 12, 14, 0, 4, -4, -2, 5, 1]
+my_list = [8, 9, 7, 0 , -2 , 3, -1, -2, 5, 12, 14, 0, 4, -4, -2, 5, 1]
 
 print("First option:")
-print(slice_less1(li, 4))
+print(slice_less1(my_list, 4))
 print("\nSecond option:")
-print(slice_less2(li, 4))
+print(slice_less2(my_list, 4))
